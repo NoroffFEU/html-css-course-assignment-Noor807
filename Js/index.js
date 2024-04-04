6b0adac52e5006aa368f2a864da7cbc178d872c4
Noor807 <152153984+Noor807@users.noreply.github.com>
2024-04-04 12:23:13 +0200

    save
    save

---
+++ Js/index.js
@@ -0,0 +1,44 @@
+//import { getToken, getApiKey } from "./api.js";
+const jacketsContainer = document.querySelector(".jackets")
+
+const baseURL = "https://api.noroff.dev/api/v1/rainy-days/"
+
+
+async function getJackets() {
+
+    
+
+  const req = await fetch(baseURL )
+
+  const result = await req.json()
+
+  for (let i = 0; i < result.length; i++){
+     jacketsContainer.innerHTML += `<a class="jacket-item" href="/html/product.html?jacketid=${result[i].id}">
+     <img src="${result[i].image}" alt="${result[i].title}"/>
+      <h3> ${result[i].title}</h3>
+      <p>${result[i].gender} </p>
+      <p> ${result[i].sizes} </p>
+      <p> ${result[i].price} </p>
+     
+     
+                         
+                       </a>` }
+ 
+ console.log(result)
+} 
+
+
+
+
+getJackets()
+
+
+
+
+
+
+
+
+
+
+ 
