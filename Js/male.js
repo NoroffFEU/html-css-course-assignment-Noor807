523a1da0a92369062b56cc8638b4156c48510b08
Noor807 <152153984+Noor807@users.noreply.github.com>
2024-04-15 21:55:56 +0200

    save

--- Js/male.js
+++ Js/male.js
@@ -2,7 +2,7 @@
  const baseURL = "https://api.noroff.dev/api/v1/rainy-days/" 
   
 
-const maleJackets = document.querySelector(".male-jackets")
+const divJackets = document.querySelector(".male-jackets")
 
 async function getMaleJackets(){
     const req = await fetch(baseURL )
@@ -16,17 +16,17 @@ async function getMaleJackets(){
         for(let i = 0; i < maleJackets.length; i++)
 
     
-    maleJackets.innerHTML += `
-       <a href="">
-            <div>
-                <div class="img-const">
-                    <img src="${maleJackets[i].image}" alt="">
-                </div>
-                <h2>${maleJackets[i].title.replace('Rainy Days', '')}</h2>
-            </div>
+    divJackets.innerHTML += `
+       <a  class="jacket-item" href="./product.html?jacketid=${maleJackets[i].id} ">
+       <img src="${maleJackets[i].image}" alt="${maleJackets[i].title}"/>
+      <h3> ${maleJackets[i].title}</h3>
+      <p>${maleJackets[i].baseColor} </p>
+      <p>${maleJackets[i].gender} </p>
+      <p> ${maleJackets[i].sizes} </p>
+      <p>$ ${maleJackets[i].price} </p>
+            
         </a>
-        
-        `
+         `
  console.log("filter on Female" , maleJackets)
 
 }
