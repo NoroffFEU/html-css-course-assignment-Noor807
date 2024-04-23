54ab022796e70487824364c058b6c1f4324fe76c
Noor807 <152153984+Noor807@users.noreply.github.com>
2024-04-23 11:43:28 +0200

    save

--- Js/checkout.js
+++ Js/checkout.js
@@ -29,7 +29,47 @@ if (localStorageList.length >0){
 const removeItem = document.querySelector('.remove')
 removeItem.addEventListener('Click',removeItemfromList)
 
-function removeItemfromList(){
-    localStorage.removeItem('jacketitem')
+function removeItemfromList(event){
+    cartItems.innerHTML = ""
+
+   const title = event.target.dataset.title
+
+   if(localStorageList.length === 1){
+
+    cartItems.innerHTML =""
+    localStorage.clear(jacketItem);
+    cartItems.innerHTML = "your cart is empty";
+   return
+
+   }
+
+   const filterOut = localStorageList.filter(jacket => jacket.title !== title)
+
+   localStorageList = filterOut
+
+   localStorageList.setItem("jacketitem", JSON.stringify(localStorageList))
+
+   for(let i= 0; i< localStorageList.length; i++)
+
+    cartItems.innerHTML += `<div class="single-item">
+    
+   <div class="product-in-cart">
+   <img class="cart-img" src="${localStorageList[i].image}" alt="${localStorageList[i].title}">
+    <h2 class="cart-h2">  ${localStorageList[i].title}</h2>
+    <p>qty:   $${localStorageList[i].quantity} </p>
+    <p>Price:   $${localStorageList[i].price} </p>
+    <button class="remove">Remove</button>
+  
+    
+    </div>        
+ </div>`
+
+   
+
 
 }
+
+
+
+
+
